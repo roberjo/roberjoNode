50f23a7d7024c6f1623ef77501d4f20abaf0101a
John B. Roberts <johnbroberts@gmail.com>
2017-12-29 00:04:02 -0500

    Updated port number to 3000

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(80, function () {
-  console.log('Magic is happening on port 80!')
+app.listen(3000, function () {
+  console.log('Magic is happening on port 3000!')
 })
